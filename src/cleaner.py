8c02bbd67cae9cdb0cf9c5d0a37e17eb4e20cd71
Daniel Boam <daniel.boam@open-innovations.org>
2022-05-19 14:34:16 +0100

    Add function to split name in to Last/First name

---
+++ src/cleaner.py
@@ -0,0 +1,23 @@
+from openpyxl import Workbook,load_workbook,utils
+
+def sepFirstLastName(wbIn,wbOut,col,row):
+    workbook = load_workbook(filename=wbIn)
+    ws = workbook.active
+
+    colint = utils.column_index_from_string(col)
+    nextCol = utils.get_column_letter(colint + 1)
+    ws.insert_cols(colint + 1)
+
+    ws[col + str(row)]  = "First Name"
+    ws[nextCol + str(row)] = "Last Name"
+
+    for i in range(row + 1,len(ws[col]) + 1):
+        name = col + str(i)
+        lastName = nextCol + str(i)
+        ws[lastName] = ws[name].value[ws[name].value.index(" ") + 1:]
+        ws[name] = ws[name].value[:ws[name].value.index(" ")]
+
+
+    workbook.save(wbOut)
+
+sepFirstLastName("Leeds 2023 Staff List.xlsx","test1.xlsx","A",8)
